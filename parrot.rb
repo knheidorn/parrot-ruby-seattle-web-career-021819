56168c3f5031ef0f74ede4a61cb9f5ba7c36ff05
Kim Heidorn <heidorkn@gmail.com>
2019-02-07 19:12:34 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(noise = "Squawk!")
-  return "#{noise}"
+  puts statement = "#{noise}"
+  return statement
 end
